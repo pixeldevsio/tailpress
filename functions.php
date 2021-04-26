f38d44f6e2fe584e912c22b10b035af6fd529374
Pixel Devs <admin@pixeldevs.io>
2021-04-26 08:53:37 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -142,6 +142,8 @@ add_action( 'widgets_init', 'tailwind_widgets_init' );
 function tailwind_scripts() {
 	wp_enqueue_style( 'tailwind-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'tailwind-style', 'rtl', 'replace' );
+	
+	wp_enqueue_style( 'tailwind-app', get_template_directory_uri() . '/css/app.css', array(), _S_VERSION );
 
 	wp_enqueue_script( 'tailwind-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
